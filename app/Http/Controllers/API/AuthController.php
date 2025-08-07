1feb0ecece8e9a591c77307c91adccc2b7cce4a4
chuchart saetiew <samchat2561@gmail.com>
2025-08-07 13:35:25 +0700

    update

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,23 +4,19 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Models\User;
-use Dotenv\Validator;
-use Illuminate\Auth\Events\Validated;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
-        $validateUser = Validator::make(
-            $request->all(),
-            [
-                'first_name' => 'required',
-                'last_name' => 'required',
-                'email' => 'required|unique:users,email',
-                'password' => 'required|confirmed',
-            ]
-        );
+        $validateUser = Validator::make($request->all(), [
+            'first_name' => 'required',
+            'last_name' => 'required',
+            'email' => 'required|unique:users,email',
+            'password' => 'required|confirmed',
+        ]);
 
         if ($validateUser->fails()) {
             return response()->json([
@@ -29,18 +25,16 @@ class AuthController extends Controller
                 'errors' => $validateUser->errors()
             ], 422);
         }
-
         $user = User::create([
             'first_name' => $request->first_name,
             'last_name' => $request->last_name,
             'email' => $request->email,
-            'password' => $request->password,
+            'password' => bcrypt($request->password),
         ]);
-
         return response()->json([
             'staus' => true,
             'message' => 'Account successfully created.',
             'user' => $user
-        ],201);
+        ], 201);
     }
 }
